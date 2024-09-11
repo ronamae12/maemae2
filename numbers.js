8d5f79ac04c90392f85c39fbdb1a0843556bfedc
Rona Mae Ancheta <ronamaeancheta18@gmail.com>
2024-09-11 21:55:22 +0800

    comoleted variables section

---
+++ numbers.js
@@ -0,0 +1,20 @@
+const firstFavNum = 10;
+const secondFavNum = 18;
+
+const sum = firstFavNum + secondFavNum;
+console.log("addition;", sum);
+
+const sub = firstFavNum - secondFavNum;
+console.log("subtraction:", sub);
+
+const product = firstFavNum * secondFavNum;
+console.log("multiplication:", product);
+
+const quotient =  firstFavNum / secondFavNum;
+console.log("division:", quotient);
+
+const modulos = firstFavNum % secondFavNum;
+console.log("modulos:", modulos);
+
+const exponent = firstFavNum ** secondFavNum;
+console.log("exponentiation:", exponent);
